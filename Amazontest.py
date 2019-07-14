53a6ffd088501e7f8617d42bf3d15b0a18ed7c1f
Slawomir <gliba@wp.pl>
2019-07-14 20:28:16 +0200

    new

--- Amazontest.py
+++ Amazontest.py
@@ -1,7 +1,22 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+import time
+
 
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get("https://www.amazon.com/")
 driver.maximize_window()
+
+# rozne metody lokalizacji elemntow
+#driver.find_element_by_xpath('//*[@id="a-autoid-0-announce"]').click()
+# driver.find_element_by_id('a-autoid-0-announce')
+# driver.find_element_by_link_text('Shop now')
+
+#driver.find_element_by_xpath('//*[@id="asin-shoveler-ns_4C58EYS6PYTZ7ZYJ6W5Q_2024_"]/div[2]//li[3]/span[1]//img')
+
+a = driver.find_element_by_xpath('//*[@alt="Nintendo Switch - Neon Red and Neon Blue Joy-Con"]').get_attribute("src")
+print(a)
+
+# for i in driver.find_elements_by_id("img"):
+#     print(i.src)
--- demo.py
+++ demo.py
@@ -1,14 +1,24 @@
+from requests import options
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-#driver = webdriver.Chrome(r"C:\Users\slawek\PycharmProjects\czyst_selenium\drivers\chromedriver.exe")
+driver = webdriver.Chrome(r"C:\Users\slawek\PycharmProjects\czyst_selenium\drivers\chromedriver.exe")
 # ...     Driver wykorzystany z uzyciem pliku w katalogu
-driver = webdriver.Chrome(ChromeDriverManager().install())
+#driver = webdriver.Chrome(ChromeDriverManager().install())
+
 driver.get(r"C:\Users\slawek\Downloads\selenium_pliki\Test.html")
 driver.maximize_window()
 #driver.find_element_by_xpath('//*[@id="newPage"]').click()
 #driver.quit()   # zamyka wszystkie okna, metoda .close() zamyka okno na ktorej byl focus
 #driver.close()
-driver.find_element(By.ID, 'fname').click()
+driver.find_element(By.ID, 'clickOnMe')
 #driver.find_element_by_id("fname").click()
+driver.find_element_by_name('fname').click()
+driver.find_element_by_link_text('IamWeirdLink')
+driver.find_element_by_xpath('//*[@id="smileImage"]')
+driver.find_element_by_class_name('topSecret')
+print(driver.find_element_by_xpath('//*[@class="tableHeader" ][text()="Month"]').text)
+driver.find_element_by_xpath('//*[@value="mercedes"]').click()
+driver.find_element_by_xpath('//*[@value="male"]').click()
+#driver.quit()
