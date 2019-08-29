30c85a8399c03c4f5ce1ac71513c32e114a6f62a
glibowsk <gliba@wp.pl>
2019-08-29 11:38:54 +0200

    new

--- double_click.py
+++ double_click.py
@@ -3,5 +3,10 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get(r"D:\Selenium\czyste_sel\selenium_pliki\Test.html")
+driver.get(r"D:\Selenium\czyste_sel\selenium_pliki\DoubleClick.html")
 driver.maximize_window()
+
+
+button = driver.find_element_by_xpath('//*[@id="bottom"]')
+
+webdriver.ActionChains(driver).double_click(button).perform()   # kilkniecie dwukrotne
