43b7c64a1e2971d2b6493f09928c4f018e499120
glibowsk <gliba@wp.pl>
2019-09-16 11:25:16 +0200

    new

---
+++ excplicit.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.get(r"D:\Selenium\czyste_sel\selenium_pliki\Waits2.html")
+driver.maximize_window()
+
+
+button = driver.find_element_by_xpath('//*[@id="clickOnMe"]')
+button.click()
+#time.sleep(5)
+print(driver.find_element_by_tag_name("p").text)
